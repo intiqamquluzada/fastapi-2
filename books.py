c6d50d41bafc166f4e83d9fe95f5ce2efa9bd2f0
intiqamquluzada <quluzadintiqam@gmail.com>
2023-10-21 17:26:33 +0400

    first-commit

---
+++ books.py
@@ -0,0 +1,51 @@
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+
+
+BOOKS = [
+
+    Book(1, 'Computer Science', 'Intigam G', 'codingWInti', 10),
+    Book(2, 'Computer Engineering', 'Samed M', 'codingWSam', 9),
+    Book(3, 'Fictional Science', 'Nijat R', 'codingWNij', 8),
+    Book(4, 'Design', 'Elshan M', 'codingWEl', 7),
+    Book(5, 'Gaming', 'Iltifat G', 'codingWIlti', 6),
+
+]
+
+
+@app.get("/")
+async def all_books():
+    return BOOKS
+
+
+@app.post("/books/create_book")
+async def create_book(book_request: BookRequest):
+    print(type(book_request))
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(new_book)
