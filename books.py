942b5e457d9e66f1d01f8b88b61078c5a332194b
intiqamquluzada <quluzadintiqam@gmail.com>
2023-11-25 12:51:35 +0400

    CRUD on FastAPI

--- books.py
+++ books.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class Book:
     id: int
     title: str
@@ -16,7 +17,6 @@ class Book:
         self.author = author
         self.description = description
         self.rating = rating
-
 
 
 class BookRequest(BaseModel):
@@ -40,18 +40,32 @@ BOOKS = [
 
 @app.get("/")
 async def all_books():
-    return BOOKS
+    return {"message": "Hello world!", "description": "First project on FastAPI"}
 
 
-@app.post("/books/create_book")
-async def create_book(book_request: BookRequest):
-    print(type(book_request))
-    new_book = Book(**book_request.model_dump())
-    BOOKS.append(new_book)
+@app.post("/create/book")
+async def create_book(name, page_count):
+    return {"name": name, "page_count": page_count}
 
 
-@app.put("/books/{title}")
-async def book_update(title: str, updated_book=BookRequest):
-    for i in range(len(BOOKS)):
-        if BOOKS[i].title.casefold() == title.casefold():
-            BOOKS[i] = updated_book
+# @app.post("/books/create_book")
+# async def create_book(book_request: BookRequest):
+#     print(type(book_request))
+#     new_book = Book(**book_request.model_dump())
+#     BOOKS.append(new_book)
+
+
+# @app.put("/books/{title}")
+# async def book_update(title: str, updated_book=BookRequest):
+#     for i in range(len(BOOKS)):
+#         if BOOKS[i].title.casefold() == title.casefold():
+#             BOOKS[i] = updated_book
+
+
+@app.put("/items/{item_id}")
+async def item_detail(item_id):
+    return {"item_id": item_id}
+
+@app.delete("/items/{item_id}")
+async def item_delete(item_id=None):
+    return {"item": item_id}
